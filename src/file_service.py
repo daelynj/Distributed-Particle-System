50a91a52d3841cb0bd09c551bb7038fc6a019584
daelynj <daelyndbjones@gmail.com>
2020-07-04 16:14:56 -0700

    Separate data into a directory

--- src/file_service.py
+++ src/file_service.py
@@ -1,6 +1,6 @@
 class FileService():
     def generate_file(self, id):
-        return open("particle_timeline_{}.txt".format(id), "w+")
+        return open("particle_timeline_data/particle_timeline_{}.txt".format(id), "w+")
 
     def append_particle_position(self, file, x, y):
         file.write("{0} {1}\n".format(x, y))
