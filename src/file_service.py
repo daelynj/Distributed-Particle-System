387499d5171bd6b26dc42dc192fb9b99499eb35a
Joel Kerfoot <joelkerfoot@gmail.com>
2020-07-05 00:05:01 -0700

    handle paths indepenant of os added additional logginf

--- src/file_service.py
+++ src/file_service.py
@@ -1,8 +1,14 @@
+import os
+
 class FileService():
     def generate_file(self, worker_id):
-        return open("src/particle_timeline_data/particle_timeline_{}.txt".format(worker_id), "w+")
+        return open(os.path.join('src', 'particle_timeline_data', f'particle_timeline_{worker_id}.txt'), 'w+')
 
-    def append_particle_data(self, file, particles):
-        for particle in particles:
-            file.write(
-                f"{particle.x} {particle.y} {particle.size} {particle.color}\n")
+    def append_particle_data(self, file, history):
+        for line in history:
+            if isinstance(line, str):
+                file.write(line)
+            else:
+                for particle in line:
+                    file.write(
+                        f"{particle.x} {particle.y} {particle.size} {particle.color}\n")
