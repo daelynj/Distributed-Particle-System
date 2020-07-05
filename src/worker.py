392eba8027d3484ccec2229103dd6047de9c17a9
daelynj <daelyndbjones@gmail.com>
2020-07-04 20:13:59 -0700

    Write particles to file

--- src/worker.py
+++ src/worker.py
@@ -8,13 +8,16 @@ from particle import Emitter, smoke_machine
 
 FILE_SERVICE = FileService()
 
-def simulate():
+
+def build_particles(particle_count):
     e = Emitter((100, 100))
-    e.add_factory(smoke_machine())
+    e.add_factory(smoke_machine(), particle_count)
 
     for i in range(10):
         e.update()
-        e.log()
+
+    return e.particles
+
 
 def get_task(stub):
     request = simulator_pb2.NextTaskInformation()
@@ -25,13 +28,13 @@ def run():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = simulator_pb2_grpc.SimulatorStub(channel)
         task = get_task(stub)
+        particle_count = 1000
 
-        simulate()
-        
         file = FILE_SERVICE.generate_file(task.id)
 
-        FILE_SERVICE.append_particle_position(file, 1, 2)
-        FILE_SERVICE.append_particle_position(file, 3, 4)
+        particles = build_particles(particle_count)
+
+        FILE_SERVICE.append_particle_data(file, particles)
 
 
 if __name__ == '__main__':
