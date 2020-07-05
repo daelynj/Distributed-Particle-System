6c1146ea023c4dab034667056fc636865e9cdf30
daelynj <daelyndbjones@gmail.com>
2020-07-04 20:13:31 -0700

    Update file_service

--- src/file_service.py
+++ src/file_service.py
@@ -2,5 +2,7 @@ class FileService():
     def generate_file(self, worker_id):
         return open("particle_timeline_data/particle_timeline_{}.txt".format(worker_id), "w+")
 
-    def append_particle_position(self, file, x_pos, y_pos):
-        file.write("{0} {1}\n".format(x_pos, y_pos))
+    def append_particle_data(self, file, particles):
+        for particle in particles:
+            file.write(
+                f"{particle.x} {particle.y} {particle.size} {particle.color}\n")
